a1898989027e1b865009641be06d33682990fc39
Zlatan Dimitrov <zlatan.dimitrov@gmail.com>
2016-08-05 16:02:52 +0300

    Fix missing parameter

--- metadata.go
+++ metadata.go
@@ -63,6 +63,7 @@ func (m *metadataDriver) Validate(meta map[string]string) (volumeMetadata, error
 	opts.FileMode = meta["filemode"]
 	opts.GID = meta["gid"]
 	opts.UID = meta["uid"]
+	opts.RemotePath = meta["remotepath"]
 
 	if meta["nolock"] == "true" {
 		opts.NoLock = true
